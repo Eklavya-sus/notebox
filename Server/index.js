54d93517bd9128bdd3560bd57d644a86be86f2b9
Eklavya-sus <95964894+Eklavya-sus@users.noreply.github.com>
2024-01-27 20:02:48 +0530

    Update index.js

--- Server/index.js
+++ Server/index.js
@@ -1,9 +1,9 @@
 
-const connectToMongo = require("./db")
-const express = require("express")
-const app = express()
-const port = 5000
-var cors = require('cors')
+const connectToMongo = require("./db");
+const express = require("express");
+const app = express();
+const port = 5000;
+var cors = require('cors');
 
 app.use(cors(
   {
@@ -14,12 +14,12 @@ app.use(cors(
 ));
 
 //Available routes 
-app.use(express.json())
+app.use(express.json());
 
-app.use("/api/auth", require("./routes/auth"))
-app.use("/api/notes", require("./routes/notes"))
+app.use("/api/auth", require("./routes/auth"));
+app.use("/api/notes", require("./routes/notes"));
 
 app.listen(port, () => {
   console.log(`Notebox-backend listening on port ${port}`)
-})
+});
 connectToMongo();
